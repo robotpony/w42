0f603272ddfc2796343a33d06d4c75ea464db6fb
Bruce Alderson <mx@warpedvisions.org>
2024-12-24 10:30:34 -0800

    Updated target server folder.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 
-hugo && rsync -avz --delete public/ w28@w42.warpedvisions.org:~/w42.warpedvisions.org/
+hugo && rsync -avz --delete public/ w28@w42.warpedvisions.org:~/warpedvisions.org/
