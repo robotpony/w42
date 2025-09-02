a88a2f9dca92fc38a9427c6be167d0e2ffb64d77
Bruce Alderson <mx@warpedvisions.org>
2025-09-02 11:00:30 -0700

    Added a simple drafts page.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/zsh
 
+hugo list drafts | ../../hd2html/hd2html.py > public/drafts.html
+
 hugo && rsync -avz --delete --exclude .gitignore --exclude .DS_Store --exclude .git/ --exclude deploy.sh public/ w28@w42.warpedvisions.org:~/warpedvisions.org/
+
