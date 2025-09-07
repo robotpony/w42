58eaa8ae4fc672dbe2dd9db4d3feb412e1fdced4
Bruce Alderson <mx@warpedvisions.org>
2025-09-07 15:35:04 -0700

    Adds a better "drafts" page.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-hugo list drafts | ../../hd2html/hd2html.py > public/drafts.html
+hugo list drafts | ../../hugo-auto-drafts-and-future-posts/hd2html.py  --markdown --title "Drafts and early posts" > content/pages/current-drafts.md
+hugo list future | ../../hugo-auto-drafts-and-future-posts/hd2html.py  --markdown --title "Future posts" > content/pages/future-posts.md
 
 hugo && rsync -avz --delete --exclude .gitignore --exclude .DS_Store --exclude .git/ --exclude deploy.sh public/ w28@w42.warpedvisions.org:~/warpedvisions.org/
 
