275c428936a465bd38cfc54f9f347630fc3b8139
Bruce Alderson <mx@warpedvisions.org>
2025-09-07 18:04:28 -0700

    Updates deploy scripts.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-hugo list drafts | ../../hugo-auto-drafts-and-future-posts/hd2html.py  --markdown --title "Drafts and early posts" > content/pages/current-drafts.md
+hugo list drafts | ../../hugo-auto-drafts-and-future-posts/hd2html.py  --markdown --title "Draft posts" > content/pages/current-drafts.md
 hugo list future | ../../hugo-auto-drafts-and-future-posts/hd2html.py  --markdown --title "Future posts" > content/pages/future-posts.md
 
 hugo && rsync -avz --delete --exclude .gitignore --exclude .DS_Store --exclude .git/ --exclude deploy.sh public/ w28@w42.warpedvisions.org:~/warpedvisions.org/
