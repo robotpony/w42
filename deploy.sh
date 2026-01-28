12d2507e4016b9b6826d548875202777204dadf0
Bruce Alderson <mx@warpedvisions.org>
2026-01-27 19:08:15 -0800

    Simplifies deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
-hugo list drafts | ../../hugo-auto-drafts-and-future-posts/hd2html.py  --markdown --title "Draft posts" > content/pages/current-drafts.md
-hugo list future | ../../hugo-auto-drafts-and-future-posts/hd2html.py  --markdown --title "Future posts" > content/pages/future-posts.md
 
 hugo && rsync -avz --delete --exclude .gitignore --exclude .DS_Store --exclude .git/ --exclude deploy.sh public/ w28@w42.warpedvisions.org:~/warpedvisions.org/
 
